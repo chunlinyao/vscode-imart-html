0ff3d2a0dcbe7234627ee34f424c0df5c414eb69
Yao Chunlin <chunlinyao@gmail.com>
2022-02-20 15:54:18 +0800

    初始版本，判断src/main/jssp来决定启用

---
+++ README.md
@@ -0,0 +1,26 @@
+# Imart Html extenson
+
+Additional support for imart tags and Javascript library.
+
+## Functionality
+
+This extension activate additional LSP when there is `src/main/jssp` folder.
+
+This Language Server works for `html` file with below functionalities:
+
+- Completions for `<imart>` HTML tags
+- Completions for Imui JS in `<script>` tag
+
+## Running the Sample
+
+- Run `npm install` in this folder. This installs all necessary npm modules in both the client and server folder
+- Open VS Code on this folder.
+- Press Ctrl+Shift+B to compile the client and server.
+- Switch to the Debug viewlet.
+- Select `Launch Client` from the drop down.
+- Run the launch config.
+- If you want to debug the server as well use the launch configuration `Attach to Server`
+- In the [Extension Development Host] instance of VSCode, open a HTML document
+  - Type `<d|` to try HTML completion
+  - Type `<script>Imui|</script>` to try JS completion
+  - Have `<script>ImuiBigDecimal</script>` to see JS Diagnostics
---
+++ client/src/extension.ts
@@ -0,0 +1,72 @@
+/* --------------------------------------------------------------------------------------------
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ * ------------------------------------------------------------------------------------------ */
+
+import * as path from 'path';
+import { ExtensionContext, Uri, workspace } from 'vscode';
+
+import {
+	LanguageClient,
+	LanguageClientOptions,
+	ServerOptions,
+	TransportKind
+} from 'vscode-languageclient';
+
+let client: LanguageClient;
+
+export async function activate(context: ExtensionContext) {
+	const root: Uri = workspace.workspaceFolders![0]!.uri;
+	let hasJssp: boolean;
+	try {
+		await workspace.fs.stat(root.with({path: path.posix.join(root.path, 'src/main/jssp')}));
+		hasJssp = true;
+	} catch {
+		hasJssp = false;
+	}
+	if (!hasJssp) {
+		return;
+	}
+	// The server is implemented in node
+	const serverModule = context.asAbsolutePath(
+		path.join('server', 'out', 'server.js')
+	);
+	// The debug options for the server
+	// --inspect=6009: runs the server in Node's Inspector mode so VS Code can attach to the server for debugging
+	const debugOptions = { execArgv: ['--nolazy', '--inspect=6009'] };
+
+	// If the extension is launched in debug mode then the debug server options are used
+	// Otherwise the run options are used
+	const serverOptions: ServerOptions = {
+		run: { module: serverModule, transport: TransportKind.ipc },
+		debug: {
+			module: serverModule,
+			transport: TransportKind.ipc,
+			options: debugOptions
+		}
+	};
+
+	// Options to control the language client
+	const clientOptions: LanguageClientOptions = {
+		// Register the server for plain text documents
+		documentSelector: [{ scheme: 'file', language: 'html' }]
+	};
+
+	// Create the language client and start the client.
+	client = new LanguageClient(
+		'languageServerImartHtml',
+		'Language Server Imart Html',
+		serverOptions,
+		clientOptions
+	);
+
+	// Start the client. This will also launch the server
+	client.start();
+}
+
+export function deactivate(): Thenable<void> | undefined {
+	if (!client) {
+		return undefined;
+	}
+	return client.stop();
+}
